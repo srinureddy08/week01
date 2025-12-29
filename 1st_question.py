b9ae2d822e7cea8025b3c134876bcbb0bc42d1c9
srinureddy08 <srinureddy08aa@gmail.com>
2025-12-29 16:01:59 +0530

    Data types

---
+++ 1st_question.py
@@ -0,0 +1,13 @@
+# Describe yourself program
+
+name = "Srinu"
+age = 16
+height = 5.6
+are_you_student = True
+
+print("----- My Details -----")
+print(f"Name            : {name}")
+print(f"Age             : {age} years")
+print(f"Height          : {height} feet")
+print(f"Are you student?: {are_you_student}")
+print("----------------------")
